e2b26bd6451df1db9439be9fd475742b5a60c4da
Yudhistira Ramandha Putra <ramandhadelputra@gmail.com>
2026-06-03 17:47:01 +0700

    fix: mengembalikan rute POST untuk membuat lokasi gudang

--- routes/warehouse_routes.go
+++ routes/warehouse_routes.go
@@ -10,7 +10,15 @@ import (
 func WarehouseRoutes(app *fiber.App) {
 	api := app.Group("/api/warehouse", middlewares.Protected())
 
-	// TAMBAHKAN KATA "/movement" DI SINI
-	api.Post("/movement", controllers.StockMovement)
+	// 1. Master Lokasi Gudang
+	api.Post("/", controllers.CreateWarehouse) // <--- INI PINTU YANG KEMARIN TERHAPUS (Penyebab 405)
 	api.Get("/", controllers.GetWarehouses)
+
+	// 2. Transaksi & Mutasi Stok
+	api.Post("/movement", controllers.StockMovement)
+
+	// 3. Fitur Lanjutan (Sesuai dengan controller asli Anda)
+	api.Post("/transfer", controllers.TransferStock)
+	api.Post("/inventory", controllers.SetInventory)
+	api.Get("/inventory", controllers.GetInventory)
 }
