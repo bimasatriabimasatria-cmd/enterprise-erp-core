78d4dc1bca91fb0994778572ab0760e4fd1e8b48
bimasatriabimasatria-cmd <bimasatriabimasatria@gmail.com>
2026-06-13 15:35:56 +0700

    Replace gin middleware with fiber middleware

--- middlewares/auth_middleware.go
+++ middlewares/auth_middleware.go
@@ -1,20 +1,19 @@
 package middlewares
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
+	"github.com/gofiber/fiber/v2"
 )
 
-// AuthMiddleware mensimulasikan login dan menyuntikkan konteks keamanan
-func AuthMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// TODO: Di masa depan (Fase 2), di sini kita akan memvalidasi JWT Token (Bearer Auth).
+// Protected mensimulasikan login dan menyuntikkan konteks keamanan
+func Protected() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		// MOCKING FASE 1:
+		// Kita paksa sistem mengira bahwa request ini dari PT ERP Maju Bersama
+		// Menggunakan UUID yang Anda dapatkan sebelumnya.
+		// Di Fiber, kita menggunakan c.Locals() untuk menyimpan data per-request.
+		c.Locals("tenant_id", "620c6b50-8e5d-4da8-8c5c-60c7c4052079")
 		
-		// UNTUK SEKARANG: Kita paksa sistem mengira bahwa user yang me-request 
-		// berasal dari PT ERP Maju Bersama (Sesuai UUID yang Anda berikan)
-		c.Set("tenant_id", "620c6b50-8e5d-4da8-8c5c-60c7c4052079")
-		
-		// Lanjut ke request berikutnya
-		c.Next()
+		// Lanjut ke request berikutnya (Controller)
+		return c.Next()
 	}
 }
