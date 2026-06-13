b28edb9acf4a47939d1bd52ba457a3174ba4dbfc
bimasatriabimasatria-cmd <bimasatriabimasatria@gmail.com>
2026-06-13 15:50:06 +0700

    Rename Protected to AuthMiddleware in auth_middleware

--- middlewares/auth_middleware.go
+++ middlewares/auth_middleware.go
@@ -4,16 +4,13 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// Protected mensimulasikan login dan menyuntikkan konteks keamanan
-func Protected() fiber.Handler {
+func AuthMiddleware() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		// MOCKING FASE 1:
-		// Kita paksa sistem mengira bahwa request ini dari PT ERP Maju Bersama
-		// Menggunakan UUID yang Anda dapatkan sebelumnya.
-		// Di Fiber, kita menggunakan c.Locals() untuk menyimpan data per-request.
+		// Mocking Tenant ID (Masukkan UUID perusahaan dummy Anda di sini)
+		// Nanti di Fase 2, nilai ini didapat dari dekripsi JWT Token.
 		c.Locals("tenant_id", "620c6b50-8e5d-4da8-8c5c-60c7c4052079")
-		
-		// Lanjut ke request berikutnya (Controller)
+
+		// Lanjutkan ke Controller (CreateJournal)
 		return c.Next()
 	}
 }
